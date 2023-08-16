5d33c30c39030bacd325ac618a8e6caacd6517d7
Courtney Gulledge <courtney.gulledge@gmail.com>
2023-08-16 16:34:03 -0400

    doubles and decimals

--- Product.cs
+++ Product.cs
@@ -2,8 +2,9 @@ public class Product
 {
     public string Name { get; set; }
     public string Color { get; set; }
-    public int Price { get; set; }
+    public decimal Price { get; set; }
     public bool Sold { get; set; }
     public DateTime StockDate { get; set; }
     public int ManufactureYear { get; set; }
+    public double Condition { get; set; }
 }
--- Program.cs
+++ Program.cs
@@ -5,64 +5,71 @@ List<Product> products = new List<Product>()
     {
         Name = "Football",
         Color= "brown",
-        Price = 15,
+        Price = 15.00M,
         Sold = false,
         StockDate = new DateTime(2022, 10, 20),
-        ManufactureYear = 2010
+        ManufactureYear = 2010,
+        Condition = 4.2
     },
     new Product()
     {
         Name = "Hockey Stick",
         Color = "yellow",
-        Price = 12,
+        Price = 12.50M,
         Sold = false,
         StockDate = new DateTime(2021, 05, 23),
-        ManufactureYear = 2019
+        ManufactureYear = 2019,
+        Condition = 3.2
     },
     new Product()
     {
         Name = "Boomerang",
         Color = "red",
-        Price = 10,
-        Sold = false,
+        Price = 10.25M,
+        Sold = true,
         StockDate = new DateTime(2023, 07, 26),
-        ManufactureYear = 2021
+        ManufactureYear = 2021,
+        Condition = 3.7
     },
     new Product()
     {
         Name = "Basketball",
         Color = "orange",
-        Price = 15,
-        Sold = true,
+        Price = 15.50M,
+        Sold = false,
         StockDate = new DateTime(2023, 06, 01),
-        ManufactureYear = 2022
+        ManufactureYear = 2022,
+        Condition = 3.8
     },
     new Product()
     {
         Name = "Frisbee",
         Color = "maroon",
-        Price = 10,
+        Price = 10.25M,
         Sold = false,
         StockDate = new DateTime(2023, 08, 11),
-        ManufactureYear = 2022
+        ManufactureYear = 2022,
+        Condition = 4.7
     },
     new Product()
     {
         Name = "Golf Club",
         Color = "silver",
-        Price = 20,
+        Price = 20.75M,
         Sold = false,
         StockDate = new DateTime(2020, 05, 10),
-        ManufactureYear = 2019
+        ManufactureYear = 2019,
+        Condition = 3.5
     },
     new Product()
     {
         Name = "Hulahoop",
         Color = "multi-colored",
-        Price = 8,
+        Price = 8.50M,
         Sold = false,
         StockDate = new DateTime(2021, 02, 25),
-        ManufactureYear = 2020
+        ManufactureYear = 2020,
+        Condition = 2.8
     }
 };
 
@@ -70,6 +77,16 @@ string greeting = @"Welcome to Thrown for a Loop!
 Your one-stop shop for used sporting equipment";
 
 Console.WriteLine(greeting);
+
+decimal totalValue = 0.0M;
+foreach (Product product in products)
+{
+    if (!product.Sold)
+    {
+        totalValue += product.Price;
+    }
+}
+Console.WriteLine($"Total inventory value: ${totalValue}");
 
 Console.WriteLine("Products:");
 for (int i = 0; i < products.Count; i++)
@@ -96,4 +113,5 @@ TimeSpan timeInStock = now - chosenProduct.StockDate;
 Console.WriteLine(@$"You chose: 
 A {chosenProduct.Color} {chosenProduct.Name}, which costs {chosenProduct.Price} dollars.
 It is {now.Year - chosenProduct.ManufactureYear} years old. 
+It's condition is rated at {chosenProduct.Condition} out of 5. 
 It {(chosenProduct.Sold ? "is not available." : $"has been in stock for {timeInStock.Days} days.")}");
